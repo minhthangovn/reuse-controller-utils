d541e10f7044670896e25d55a46f2be7e5f49408
Ly Minh <minduc2020@gmail.com>
2023-05-22 15:10:07 +0700

    Initialize source

---
+++ src/types.ts
@@ -0,0 +1,29 @@
+/**
+ * Human-readable network name
+ */
+export type NetworkType =
+  | 'kovan'
+  | 'localhost'
+  | 'mainnet'
+  | 'rinkeby'
+  | 'goerli'
+  | 'ropsten'
+  | 'rpc';
+
+export enum NetworksChainId {
+  mainnet = '1',
+  kovan = '42',
+  rinkeby = '4',
+  goerli = '5',
+  ropsten = '3',
+  localhost = '',
+  rpc = '',
+}
+
+export type Json =
+  | null
+  | boolean
+  | number
+  | string
+  | Json[]
+  | { [prop: string]: Json };
